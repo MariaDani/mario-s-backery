8c251df3e5e01694c49c2d04c45f878c95bb1b7b
MariaDani <52234314+MariaDani@users.noreply.github.com>
2022-03-27 16:49:09 +0300

    Update main.js

--- _bakery-app/src/main.js
+++ _bakery-app/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './vuex/store'
 import router from './router/router'
-import './assets/styles/styles.scss'
+//import './assets/styles/styles.scss'
 import 'material-design-icons-iconfont'
 
 
